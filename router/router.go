d32b633b50357b7e0288e9e049afaca3ed31bd09
Manikanta Reddy Palle <pmani9505@gmail.com>
2023-09-10 14:55:12 +0530

    first commit

---
+++ router/router.go
@@ -0,0 +1,19 @@
+package router
+
+import (
+	"github.com/V31L0x1/mongoapi/controller"
+	"github.com/gorilla/mux"
+)
+
+func Router() *mux.Router {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/", controller.ServeHome).Methods("GET")
+	router.HandleFunc("/api/movies", controller.GetMyAllMovies).Methods("GET")
+	router.HandleFunc("/api/movie", controller.CreateMovie).Methods("POST")
+	router.HandleFunc("/api/movie/{id}", controller.MarkAsWatched).Methods("PUT")
+	router.HandleFunc("/api/movie/{id}", controller.DeleteAMovie).Methods("DELETE")
+	router.HandleFunc("/api/deleteallmovies", controller.DeleteAllMovies).Methods("DELETE")
+
+	return router
+}
